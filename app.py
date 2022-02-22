a4b4c9333897cca0b63511a6ad87dab20b93087d
Manish Jaiswal <mjaiswal@cisco.com>
2022-02-22 21:47:27 +0530

    refactored

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 
 from security import authenticate, identity
+from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'mjaiswal'
@@ -10,54 +12,9 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-items = []
-
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price', type=float, required=True, help='This cannot be blank!')
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'Item': item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': f'An item with name {name} already exists'}, 400
-
-        data = Item.parser.parse_args()
-        new_item = {
-            'name': name,
-            'price': data['price']
-        }
-        items.append(new_item)
-        return new_item, 201
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] == name, items))
-        return {'message': f'Item {name} is deleted'}
-
-    def put(self, name):
-        data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item:
-            item.update(data)
-        else:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        return item
-
-
-class ItemList(Resource):
-
-    def get(self):
-        return {'Items': items}
-
-
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
