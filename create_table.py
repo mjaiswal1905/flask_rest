86e2b47b0f1ec1da67948703a54a35a1dd2fcf42
Manish Jaiswal <mjaiswal@cisco.com>
2022-02-22 21:46:45 +0530

    typo

--- create_table.py
+++ create_table.py
@@ -4,10 +4,10 @@ connection = sqlite3.connect('data/data.db')
 cursor = connection.cursor()
 
 # INTEGER -> auto-incrementing columns
-query_create = "CREATE TABLE IF NOT EXISTS users VALUES (id INTEGER PRIMARY KEY, username text, password text)"
+query_create = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(query_create)
 
-query_create_2 = "CREATE TABLE IF NOT EXISTS items VALUES (items text PRIMARY KEY, price real)"
+query_create_2 = "CREATE TABLE IF NOT EXISTS items (name text PRIMARY KEY, price real)"
 cursor.execute(query_create_2)
 
 connection.commit()
